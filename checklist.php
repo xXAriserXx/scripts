c0feb221f4f3ee4b2ffe665a09ff8c1dd7499686
xXAriserXx <j.r.precilla365@gmail.com>
2025-09-14 21:39:41 +0200

    first script, the php checklist

---
+++ checklist.php
@@ -0,0 +1,31 @@
+<?php
+
+function getKey(): string {
+    if (strncasecmp(PHP_OS, 'WIN', 3) !== 0) {
+        system('stty cbreak -echo');
+        $char = fgetc(STDIN);
+        system('stty sane');
+    } else { 
+        $char = trim(shell_exec('choice /c YN /n'));
+    }
+    return strtolower($char);
+}
+
+$questions = [
+    "Did you delete all the console logs?",
+    "Did you change the version of the project?",
+    "Did you check if the build is building?"
+];
+
+foreach ($questions as $q) {
+    echo "$q [y/n]: ";
+    $input = getKey();
+    echo "$input\n"; // show what was pressed
+    if ($input !== 'y') {
+        echo "You are not ready \n";
+        exit;
+    }
+}
+
+echo "Congrats, you are ready \n";
+
